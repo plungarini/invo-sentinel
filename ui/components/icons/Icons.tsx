ae4b0169eddd3e381cbf0e696a509631dbe4111a
plungarini <pietro@lungarini.it>
2026-08-20 12:36:14 +0200

    feat(settings): add settings icon and undismissible modal mode for setup wizard

--- ui/components/icons/Icons.tsx
+++ ui/components/icons/Icons.tsx
@@ -3,10 +3,16 @@ export interface IconProps {
 	strokeWidth?: number;
 }
 
-export function HomeIcon({ className = "h-5 w-5", strokeWidth = 1.5 }: IconProps) {
+export function HomeIcon({ className = 'h-5 w-5', strokeWidth = 1.5 }: IconProps) {
 	return (
 		<svg className={className} viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-			<path d="M9.07874 16.1354H14.8937" stroke="currentColor" strokeWidth={strokeWidth} strokeLinecap="round" strokeLinejoin="round" />
+			<path
+				d="M9.07874 16.1354H14.8937"
+				stroke="currentColor"
+				strokeWidth={strokeWidth}
+				strokeLinecap="round"
+				strokeLinejoin="round"
+			/>
 			<path
 				fillRule="evenodd"
 				clipRule="evenodd"
@@ -20,7 +26,7 @@ export function HomeIcon({ className = "h-5 w-5", strokeWidth = 1.5 }: IconProps
 	);
 }
 
-export function ActivityIcon({ className = "h-5 w-5", strokeWidth = 1.5 }: IconProps) {
+export function ActivityIcon({ className = 'h-5 w-5', strokeWidth = 1.5 }: IconProps) {
 	return (
 		<svg className={className} viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
 			<path
@@ -50,7 +56,7 @@ export function ActivityIcon({ className = "h-5 w-5", strokeWidth = 1.5 }: IconP
 	);
 }
 
-export function WalletIcon({ className = "h-5 w-5", strokeWidth = 1.5 }: IconProps) {
+export function WalletIcon({ className = 'h-5 w-5', strokeWidth = 1.5 }: IconProps) {
 	return (
 		<svg className={className} viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
 			<path
@@ -60,8 +66,20 @@ export function WalletIcon({ className = "h-5 w-5", strokeWidth = 1.5 }: IconPro
 				strokeLinecap="round"
 				strokeLinejoin="round"
 			/>
-			<path d="M17.7221 12.0532H17.4249" stroke="currentColor" strokeWidth={strokeWidth} strokeLinecap="round" strokeLinejoin="round" />
-			<path d="M7.6062 8.14367H11.6662" stroke="currentColor" strokeWidth={strokeWidth} strokeLinecap="round" strokeLinejoin="round" />
+			<path
+				d="M17.7221 12.0532H17.4249"
+				stroke="currentColor"
+				strokeWidth={strokeWidth}
+				strokeLinecap="round"
+				strokeLinejoin="round"
+			/>
+			<path
+				d="M7.6062 8.14367H11.6662"
+				stroke="currentColor"
+				strokeWidth={strokeWidth}
+				strokeLinecap="round"
+				strokeLinejoin="round"
+			/>
 			<path
 				fillRule="evenodd"
 				clipRule="evenodd"
@@ -75,7 +93,7 @@ export function WalletIcon({ className = "h-5 w-5", strokeWidth = 1.5 }: IconPro
 	);
 }
 
-export function WrenchToolIcon({ className = "h-5 w-5", strokeWidth = 1.5 }: IconProps) {
+export function WrenchToolIcon({ className = 'h-5 w-5', strokeWidth = 1.5 }: IconProps) {
 	return (
 		<svg className={className} viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
 			<path
@@ -89,7 +107,7 @@ export function WrenchToolIcon({ className = "h-5 w-5", strokeWidth = 1.5 }: Ico
 	);
 }
 
-export function RestartIcon({ className = "h-5 w-5", strokeWidth = 1.5 }: IconProps) {
+export function RestartIcon({ className = 'h-5 w-5', strokeWidth = 1.5 }: IconProps) {
 	return (
 		<svg className={className} viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
 			<path
@@ -110,7 +128,7 @@ export function RestartIcon({ className = "h-5 w-5", strokeWidth = 1.5 }: IconPr
 	);
 }
 
-export function HeartRateIcon({ className = "h-5 w-5", strokeWidth = 1.5 }: IconProps) {
+export function HeartRateIcon({ className = 'h-5 w-5', strokeWidth = 1.5 }: IconProps) {
 	return (
 		<svg className={className} viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
 			<path
@@ -120,9 +138,27 @@ export function HeartRateIcon({ className = "h-5 w-5", strokeWidth = 1.5 }: Icon
 				strokeLinecap="round"
 				strokeLinejoin="round"
 			/>
-			<path d="M7.05469 20.7188H16.943" stroke="currentColor" strokeWidth={strokeWidth} strokeLinecap="round" strokeLinejoin="round" />
-			<path d="M9.88339 16.7969L9.24609 20.7141" stroke="currentColor" strokeWidth={strokeWidth} strokeLinecap="round" strokeLinejoin="round" />
-			<path d="M14.1152 16.7969L14.7525 20.7141" stroke="currentColor" strokeWidth={strokeWidth} strokeLinecap="round" strokeLinejoin="round" />
+			<path
+				d="M7.05469 20.7188H16.943"
+				stroke="currentColor"
+				strokeWidth={strokeWidth}
+				strokeLinecap="round"
+				strokeLinejoin="round"
+			/>
+			<path
+				d="M9.88339 16.7969L9.24609 20.7141"
+				stroke="currentColor"
+				strokeWidth={strokeWidth}
+				strokeLinecap="round"
+				strokeLinejoin="round"
+			/>
+			<path
+				d="M14.1152 16.7969L14.7525 20.7141"
+				stroke="currentColor"
+				strokeWidth={strokeWidth}
+				strokeLinecap="round"
+				strokeLinejoin="round"
+			/>
 			<path
 				d="M6.73242 10.5028H9.43145L10.5027 7.29688L12.4486 13.1221L14.2613 9.09882L15.0961 10.5028H17.2639"
 				stroke="currentColor"
@@ -134,7 +170,7 @@ export function HeartRateIcon({ className = "h-5 w-5", strokeWidth = 1.5 }: Icon
 	);
 }
 
-export function KeyIcon({ className = "h-5 w-5", strokeWidth = 1.5 }: IconProps) {
+export function KeyIcon({ className = 'h-5 w-5', strokeWidth = 1.5 }: IconProps) {
 	return (
 		<svg className={className} viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
 			<path
@@ -157,7 +193,7 @@ export function KeyIcon({ className = "h-5 w-5", strokeWidth = 1.5 }: IconProps)
 	);
 }
 
-export function InboxDownIcon({ className = "h-5 w-5", strokeWidth = 1.5 }: IconProps) {
+export function InboxDownIcon({ className = 'h-5 w-5', strokeWidth = 1.5 }: IconProps) {
 	return (
 		<svg className={className} viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
 			<path
@@ -174,13 +210,25 @@ export function InboxDownIcon({ className = "h-5 w-5", strokeWidth = 1.5 }: Icon
 				strokeLinecap="round"
 				strokeLinejoin="round"
 			/>
-			<path d="M14.9364 7.56641L12.1894 10.2163L9.44336 7.56641" stroke="currentColor" strokeWidth={strokeWidth} strokeLinecap="round" strokeLinejoin="round" />
-			<path d="M12.1895 10.2133V3" stroke="currentColor" strokeWidth={strokeWidth} strokeLinecap="round" strokeLinejoin="round" />
+			<path
+				d="M14.9364 7.56641L12.1894 10.2163L9.44336 7.56641"
+				stroke="currentColor"
+				strokeWidth={strokeWidth}
+				strokeLinecap="round"
+				strokeLinejoin="round"
+			/>
+			<path
+				d="M12.1895 10.2133V3"
+				stroke="currentColor"
+				strokeWidth={strokeWidth}
+				strokeLinecap="round"
+				strokeLinejoin="round"
+			/>
 		</svg>
 	);
 }
 
-export function InboxUpIcon({ className = "h-5 w-5", strokeWidth = 1.5 }: IconProps) {
+export function InboxUpIcon({ className = 'h-5 w-5', strokeWidth = 1.5 }: IconProps) {
 	return (
 		<svg className={className} viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
 			<path
@@ -190,8 +238,20 @@ export function InboxUpIcon({ className = "h-5 w-5", strokeWidth = 1.5 }: IconPr
 				strokeLinecap="round"
 				strokeLinejoin="round"
 			/>
-			<path d="M9.25586 5.64932L12.0023 3L14.7478 5.64932" stroke="currentColor" strokeWidth={strokeWidth} strokeLinecap="round" strokeLinejoin="round" />
-			<path d="M12 3.00391V10.2157" stroke="currentColor" strokeWidth={strokeWidth} strokeLinecap="round" strokeLinejoin="round" />
+			<path
+				d="M9.25586 5.64932L12.0023 3L14.7478 5.64932"
+				stroke="currentColor"
+				strokeWidth={strokeWidth}
+				strokeLinecap="round"
+				strokeLinejoin="round"
+			/>
+			<path
+				d="M12 3.00391V10.2157"
+				stroke="currentColor"
+				strokeWidth={strokeWidth}
+				strokeLinecap="round"
+				strokeLinejoin="round"
+			/>
 			<path
 				d="M20.8017 13.3086H16.6087C15.7402 13.3086 14.9479 13.8 14.5622 14.578C14.0946 15.5199 13.1223 16.1684 11.9986 16.1684C10.8748 16.1684 9.90252 15.5199 9.43494 14.578C9.04925 13.8 8.25693 13.3086 7.38842 13.3086H3.19922"
 				stroke="currentColor"
@@ -203,7 +263,7 @@ export function InboxUpIcon({ className = "h-5 w-5", strokeWidth = 1.5 }: IconPr
 	);
 }
 
-export function ReceiptEditIcon({ className = "h-5 w-5", strokeWidth = 1.5 }: IconProps) {
+export function ReceiptEditIcon({ className = 'h-5 w-5', strokeWidth = 1.5 }: IconProps) {
 	return (
 		<svg className={className} viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
 			<path
@@ -213,7 +273,13 @@ export function ReceiptEditIcon({ className = "h-5 w-5", strokeWidth = 1.5 }: Ic
 				strokeLinecap="round"
 				strokeLinejoin="round"
 			/>
-			<path d="M13.6139 8.90625H8.75781M11.9949 12.6784H8.75781" stroke="currentColor" strokeWidth={strokeWidth} strokeLinecap="round" strokeLinejoin="round" />
+			<path
+				d="M13.6139 8.90625H8.75781M11.9949 12.6784H8.75781"
+				stroke="currentColor"
+				strokeWidth={strokeWidth}
+				strokeLinecap="round"
+				strokeLinejoin="round"
+			/>
 			<path
 				fillRule="evenodd"
 				clipRule="evenodd"
@@ -227,7 +293,7 @@ export function ReceiptEditIcon({ className = "h-5 w-5", strokeWidth = 1.5 }: Ic
 	);
 }
 
-export function BarChartAiIcon({ className = "h-5 w-5", strokeWidth = 1.5 }: IconProps) {
+export function BarChartAiIcon({ className = 'h-5 w-5', strokeWidth = 1.5 }: IconProps) {
 	return (
 		<svg className={className} viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
 			<path
@@ -258,6 +324,30 @@ export function BarChartAiIcon({ className = "h-5 w-5", strokeWidth = 1.5 }: Ico
 				strokeLinecap="round"
 				strokeLinejoin="round"
 			/>
+		</svg>
+	);
+}
+
+export function SettingsIcon({ className = 'h-5 w-5', strokeWidth = 1.5 }: IconProps) {
+	return (
+		<svg className={className} viewBox="0 0 24 24" version="1.1" xmlns="http://www.w3.org/2000/svg">
+			<g
+				id="Iconly/Light/Setting"
+				stroke="none"
+				strokeWidth={strokeWidth}
+				fill="none"
+				fillRule="evenodd"
+				strokeLinecap="round"
+				strokeLinejoin="round"
+			>
+				<g transform="translate(2.500000, 1.500000)" stroke="currentColor" strokeWidth={strokeWidth}>
+					<path
+						d="M18.3066362,6.12356982 L17.6842106,5.04347829 C17.1576365,4.12955711 15.9906873,3.8142761 15.0755149,4.33867279 L15.0755149,4.33867279 C14.6398815,4.59529992 14.1200613,4.66810845 13.6306859,4.54104256 C13.1413105,4.41397667 12.7225749,4.09747295 12.4668193,3.66132725 C12.3022855,3.38410472 12.2138742,3.06835005 12.2105264,2.74599544 L12.2105264,2.74599544 C12.2253694,2.22917739 12.030389,1.72835784 11.6700024,1.3576252 C11.3096158,0.986892553 10.814514,0.777818938 10.2974829,0.778031878 L9.04347831,0.778031878 C8.53694532,0.778031878 8.05129106,0.97987004 7.69397811,1.33890085 C7.33666515,1.69793166 7.13715288,2.18454839 7.13958814,2.69107553 L7.13958814,2.69107553 C7.12457503,3.73688099 6.27245786,4.57676682 5.22654465,4.57665906 C4.90419003,4.57331126 4.58843537,4.48489995 4.31121284,4.32036615 L4.31121284,4.32036615 C3.39604054,3.79596946 2.22909131,4.11125048 1.70251717,5.02517165 L1.03432495,6.12356982 C0.508388616,7.03634945 0.819378585,8.20256183 1.72997713,8.73226549 L1.72997713,8.73226549 C2.32188101,9.07399614 2.68650982,9.70554694 2.68650982,10.3890161 C2.68650982,11.0724852 2.32188101,11.704036 1.72997713,12.0457667 L1.72997713,12.0457667 C0.820534984,12.5718952 0.509205679,13.7352837 1.03432495,14.645309 L1.03432495,14.645309 L1.6659039,15.7345539 C1.9126252,16.1797378 2.3265816,16.5082503 2.81617164,16.6473969 C3.30576167,16.7865435 3.83061824,16.7248517 4.27459956,16.4759726 L4.27459956,16.4759726 C4.71105863,16.2212969 5.23116727,16.1515203 5.71931837,16.2821523 C6.20746948,16.4127843 6.62321383,16.7330005 6.87414191,17.1716248 C7.03867571,17.4488473 7.12708702,17.764602 7.13043482,18.0869566 L7.13043482,18.0869566 C7.13043482,19.1435014 7.98693356,20.0000001 9.04347831,20.0000001 L10.2974829,20.0000001 C11.3504633,20.0000001 12.2054882,19.1490783 12.2105264,18.0961099 L12.2105264,18.0961099 C12.2080776,17.5879925 12.4088433,17.0999783 12.7681408,16.7406809 C13.1274382,16.3813834 13.6154524,16.1806176 14.1235699,16.1830664 C14.4451523,16.1916732 14.7596081,16.2797208 15.0389017,16.4393593 L15.0389017,16.4393593 C15.9516813,16.9652957 17.1178937,16.6543057 17.6475973,15.7437072 L17.6475973,15.7437072 L18.3066362,14.645309 C18.5617324,14.2074528 18.6317479,13.6859659 18.5011783,13.1963297 C18.3706086,12.7066935 18.0502282,12.2893121 17.6109841,12.0366133 L17.6109841,12.0366133 C17.17174,11.7839145 16.8513595,11.3665332 16.7207899,10.876897 C16.5902202,10.3872608 16.6602358,9.86577384 16.9153319,9.42791767 C17.0812195,9.13829096 17.3213574,8.89815312 17.6109841,8.73226549 L17.6109841,8.73226549 C18.5161253,8.20284891 18.8263873,7.04344892 18.3066362,6.13272314 L18.3066362,6.13272314 L18.3066362,6.12356982 Z"
+						id="Path_33946"
+					></path>
+					<circle id="Ellipse_737" cx="9.67505726" cy="10.3890161" r="2.63615562"></circle>
+				</g>
+			</g>
 		</svg>
 	);
 }
--- ui/components/shared/Modal.tsx
+++ ui/components/shared/Modal.tsx
@@ -8,11 +8,13 @@ export default function Modal({
 	title,
 	children,
 }: {
-	onClose: () => void;
+	/** Omit for an undismissible modal (no close button, no Escape/backdrop-click dismiss) - e.g. the first-run setup wizard, which has nothing to dismiss to yet. */
+	onClose?: () => void;
 	title?: string;
 	children: React.ReactNode;
 }) {
 	useEffect(() => {
+		if (!onClose) return;
 		const onKeyDown = (e: KeyboardEvent) => {
 			if (e.key === "Escape") onClose();
 		};
@@ -33,17 +35,21 @@ export default function Modal({
 				className="animate-modal-content scrollbar-thin max-h-[88vh] w-full max-w-[560px] overflow-y-auto rounded-t-2xl bg-card sm:rounded-2xl"
 				onClick={(e) => e.stopPropagation()}
 			>
-				<div className="sticky top-0 z-10 flex items-center justify-between gap-3 bg-card/90 px-5 py-4 backdrop-blur-xl">
-					<span className="text-[15px] font-semibold tracking-tight">{title ?? ""}</span>
-					<button
-						onClick={onClose}
-						className="cursor-pointer rounded-full bg-surface p-2 text-text-muted transition-all duration-150 hover:bg-surface-hover hover:text-text active:scale-95"
-						aria-label="Close"
-					>
-						<X className="h-4 w-4" strokeWidth={2.5} />
-					</button>
-				</div>
-				<div className="px-5 pb-6">{children}</div>
+				{(title || onClose) && (
+					<div className="sticky top-0 z-10 flex items-center justify-between gap-3 bg-card/90 px-5 py-4 backdrop-blur-xl">
+						<span className="text-[15px] font-semibold tracking-tight">{title ?? ""}</span>
+						{onClose && (
+							<button
+								onClick={onClose}
+								className="cursor-pointer rounded-full bg-surface p-2 text-text-muted transition-all duration-150 hover:bg-surface-hover hover:text-text active:scale-95"
+								aria-label="Close"
+							>
+								<X className="h-4 w-4" strokeWidth={2.5} />
+							</button>
+						)}
+					</div>
+				)}
+				<div className={`px-5 pb-6 ${title || onClose ? "" : "pt-5"}`}>{children}</div>
 			</div>
 		</div>
 	);
